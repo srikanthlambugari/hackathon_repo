5d018a849206c80ec1b63ef2753cdc7d0d4e68c2
Srikanth Lambugari <41077221+srikanthlambugari@users.noreply.github.com>
2020-02-06 15:04:17 +0530

    Delete devreply.rb

--- devreply.rb
+++
@@ -1,8 +0,0 @@
-widget_link = @context.page.send(Formatters.string_to_snake_case(widget_id)).get_links
-  devices_added_count = nil
-  widget_link.each do|list_item|
-    if list_item.span(class: 'ng-binding').present?
-      devices_added_count = list_item.span(class: 'ng-binding').text.to_i
-      a+++
-    end
-  end
